5a065bc9d313f05620b8ad8d7a978af68c75c9bb
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-07-21 23:46:18 +0400

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,9 @@
+name = input("ra gqvia?: ")
+surname = input("ra gvaris xar?: ")
+age = input("sheni asaki?: ")
+height = input("ramdeni xar simagleshi?: ")
+favfood = input("ra aris sheni sayvareli sawmeli?: ")
+favsport = input("romeli sporti gizidavs yvelaze metad?: ")
+
+print("gamarjoba " + name + " " + surname + "," + " " + "sheni asakia " + age + ", shen xar " + height + " simaglis." 
++ " sheni sayvareli sawmelia " + favfood + ", da sheni sayvareli sportia " + favsport + ".")
---
+++ day4vol2.py
@@ -0,0 +1,12 @@
+name = input("ra gqvia?: ")
+surname = input("ra gvaris xar?: ")
+age = input("sheni asaki?: ")
+height = input("ramdeni xar simagleshi?: ")
+favfood = input("ra aris sheni sayvareli sawmeli?: ")
+favsport = input("romeli sporti gizidavs yvelaze metad?: ")
+
+name_surname = "gamarjoba " + name + " " + surname + ","
+age_height = "sheni asakia " + age + " weli" + ", shen xar " + height + " simaglis."
+fav_info = "sheni sayvareli sawmelia " + favfood + ", da sheni sayvareli sportia " + favsport + "."
+
+print(name_surname, age_height, fav_info)
